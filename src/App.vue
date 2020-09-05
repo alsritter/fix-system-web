d8477754cc12a6646c5d84571a8c548309b6ea46
kujoun <1196120340@qq.com>
2020-09-06 00:19:30 +0800

    模板搭建

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <Nav></Nav>
+    <div id="navi">
+      <Nav></Nav>
+    </div>
+    <div id="content">
+    </div>
   </div>
 </template>
 
@@ -16,4 +20,16 @@ export default {
 
 <style>
 
+  #navi {
+    background-image: linear-gradient(40deg, #8AC6D1, #BBDED7);
+    width: 30%;
+    height: 1000px;
+    float: left;
+  }
+  #content {
+    background-color: pink;
+    width: 70%;
+    height: 1000px;
+    float: right;
+  }
 </style>
--- src/components/admin/nav.vue
+++ src/components/admin/nav.vue
@@ -1,14 +1,40 @@
 
 <template>
     <div>
-        <el-row>
-        <el-button>默认按钮</el-button>
-        <el-button type="primary">主要按钮</el-button>
-        <el-button type="success">成功按钮</el-button>
-        <el-button type="info">信息按钮</el-button>
-        <el-button type="warning">警告按钮</el-button>
-        <el-button type="danger">危险按钮</el-button>
-      </el-row>
+      <el-col id="navi-menu" :span="12">
+    <h5>晚上好！管理员123</h5>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+       <el-menu-item index="1" class="navi-child-item">
+        <i class="el-icon-menu" ></i>
+        <span slot="title">学生管理</span>
+      </el-menu-item>
+      <el-menu-item index="2" class="navi-child-item">
+        <i class="el-icon-menu"></i>
+        <span slot="title">工人管理</span>
+      </el-menu-item>
+      <el-menu-item index="3" class="navi-child-item">
+        <i class="el-icon-setting"></i>
+        <span slot="title">耗材管理</span>
+      </el-menu-item>
+      <el-menu-item index="4" class="navi-child-item">
+        <i class="el-icon-setting"></i>
+        <span slot="title">待处理的订单</span>
+      </el-menu-item>
+       <el-menu-item index="5" class="navi-child-item">
+        <i class="el-icon-setting"></i>
+        <span slot="title">进行中的订单</span>
+      </el-menu-item>
+       <el-menu-item index="6" class="navi-child-item">
+        <i class="el-icon-setting"></i>
+        <span slot="title">已完成的订单</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+    </div>
 </template>
 
 <script>
@@ -17,5 +43,15 @@ export default {
 </script>
 
 <style>
-
+ .el-menu-vertical-demo {
+   background-color: #8AC6D1;
+ }
+ #navi-menu{
+   width: 100%;
+   border: none;
+ }
+ .navi-child-item{
+   border: 10px;
+  background-color: #BBDED7;
+ }
 </style>
